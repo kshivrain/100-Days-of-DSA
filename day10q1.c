3fb2c5f48a3166fc249881c24973af42d408a7aa
kshivrain <kshivrain@gmail.com>
2026-02-10 17:30:19 +0530

    Add files via upload

---
+++ day10q1.c
@@ -0,0 +1,25 @@
+/*Problem: Read a string and check if it is a palindrome using two-pointer comparison.*/
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char s[1000];
+
+    printf("Enter the string: ");
+    scanf("%s", s);
+
+    int left = 0;
+    int right = strlen(s) - 1;
+
+    while (left < right) {
+        if (s[left] != s[right]) {
+            printf("NO");
+            return 0;
+        }
+        left++;
+        right--;
+    }
+
+    printf("YES");
+    return 0;
+}
